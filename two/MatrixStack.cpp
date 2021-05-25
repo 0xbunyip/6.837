db2800b65680d740ddbd734a745c0987b44fc971
baodc <chiemduybao@gmail.com>
2021-05-25 07:09:02 +0200

    Implement matrix stack.

--- two/MatrixStack.cpp
+++ two/MatrixStack.cpp
@@ -1,29 +1,34 @@
 #include "MatrixStack.h"
+#include "GL/freeglut.h"
 
-MatrixStack::MatrixStack()
-{
-	// Initialize the matrix stack with the identity matrix.
+MatrixStack::MatrixStack() {
+  // Initialize the matrix stack with the identity matrix.
+  m_matrices.push_back(Matrix4f::identity());
+  glLoadMatrixf(m_matrices.back());
 }
 
-void MatrixStack::clear()
-{
-	// Revert to just containing the identity matrix.
+void MatrixStack::clear() {
+  // Revert to just containing the identity matrix.
+  m_matrices.clear();
+  m_matrices.push_back(Matrix4f::identity());
+  glLoadMatrixf(m_matrices.back());
 }
 
-Matrix4f MatrixStack::top()
-{
-	// Return the top of the stack
-	return Matrix4f();
+Matrix4f MatrixStack::top() {
+  // Return the top of the stack
+  return m_matrices.back();
 }
 
-void MatrixStack::push( const Matrix4f& m )
-{
-	// Push m onto the stack.
-	// Your stack should have OpenGL semantics:
-	// the new top should be the old top multiplied by m
+void MatrixStack::push(const Matrix4f &m) {
+  // Push m onto the stack.
+  // Your stack should have OpenGL semantics:
+  // the new top should be the old top multiplied by m
+  m_matrices.push_back(m_matrices.back() * m);
+  glLoadMatrixf(m_matrices.back());
 }
 
-void MatrixStack::pop()
-{
-	// Remove the top element from the stack
+void MatrixStack::pop() {
+  // Remove the top element from the stack
+  m_matrices.pop_back();
+  glLoadMatrixf(m_matrices.back());
 }
