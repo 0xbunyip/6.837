8b1cf50128ac18cafe9175d6da1abe70b4e3295d
baodc <chiemduybao@gmail.com>
2021-06-08 07:41:56 +0200

    Implement Particle.

--- three/particle.cpp
+++ three/particle.cpp
@@ -1,16 +1,18 @@
 #include "particle.hpp"
 
-Vector3f &Particle::p() { return p_; }
-Vector3f &Particle::v() { return v_; }
+#include <memory>
+#include <vecmath/vecmath.h>
+
+// Vector3f& Particle::p() { return p_; }
+// Vector3f& Particle::v() { return v_; }
 
 std::unique_ptr<Particle> Particle::Copy(const Vector3f &p, const Vector3f &v) {
-  std::unique_ptr<Particle> part = new Particle(p, m_);
-  part.v_ = v;
+  std::unique_ptr<Particle> part(new Particle(p, v, m_));
   return part;
 }
 
-Vector3f &FixedParticle::p() {
-  p2_ = p;
+Vector3f& FixedParticle::p() {
+  p2_ = p_;
   return p2_;
 }
 
@@ -21,11 +23,13 @@ Vector3f &FixedParticle::v() {
 
 std::unique_ptr<Particle> FixedParticle::Copy(const Vector3f &p,
                                               const Vector3f &v) {
-  Particle part;
-  part.p_ = p;
-  part.v_ = v;
-  part.m_ = m_;
-  part.kv_ = kv_;
+  std::unique_ptr<Particle> part(new FixedParticle(p));
+  return part;
+}
+
+std::unique_ptr<Particle> VParticle::Copy(const Vector3f &p,
+                                          const Vector3f &v) {
+  std::unique_ptr<Particle> part(new VParticle(p, v, m_, kv_));
   return part;
 }
 
--- three/particle.hpp
+++ three/particle.hpp
@@ -2,34 +2,41 @@
 #define PARTICLE_H
 
 #include <vecmath/vecmath.h>
+#include <memory>
 
 constexpr float G_CONSTANT = 9.81;
-constexpr Vector3f G_DIR = Vector3f(0, -1, 0);
-constexpr Vector3f GRAVITY = G_CONSTANT * G_DIR;
+// constexpr Vector3f G_DIR = Vector3f(0, -1, 0);
+// constexpr Vector3f GRAVITY = G_CONSTANT * G_DIR;
 
 class Particle {
 public:
-  Particle(const Vector3f& p, const float m): p_(p), m_(m) {}
-  Vector3f& p() { return p_; }
-  Vector3f& v() { return v_; }
+  Particle(const Vector3f& p, const Vector3f& v, const float m): p_(p), v_(v), m_(m) {}
 
-  std::unique_ptr<Particle> Copy(const Vector3f &p, const Vector3f &v);
+  virtual Vector3f& p();
+  virtual Vector3f& v();
+  virtual std::unique_ptr<Particle> Copy(const Vector3f &p, const Vector3f &v);
 
-private:
+protected:
   Vector3f p_, v_;
   float m_;
 };
 
 class FixedParticle : public Particle {
 public:
-  FixedParticle(const Vector3f& p): p_(p), m_(1) {}
+  FixedParticle(const Vector3f& p): Particle(p, Vector3f::ZERO, 1) {}
+
+  Vector3f& p() override;
+  Vector3f& v() override;
+  std::unique_ptr<Particle> Copy(const Vector3f &p, const Vector3f &v) override;
 
 private:
   Vector3f p2_, v2_;
-}
+};
 
 class VParticle : public Particle {
-  VParticle(const Vector3f& p, const float m, const float kv): Particle(p, m), kv_(kv) {}
+  VParticle(const Vector3f& p, const Vector3f& v, const float m, const float kv): Particle(p, v, m), kv_(kv) {}
+
+  std::unique_ptr<Particle> Copy(const Vector3f &p, const Vector3f &v) override;
 
 private:
   float kv_;
