2ae1b643c9e7b658bb34ebf908d4b004a8b373b0
baodc <chiemduybao@gmail.com>
2021-06-24 19:10:00 +0200

    Implement sphere.

--- four/Ray.h
+++ four/Ray.h
@@ -11,51 +11,34 @@ using namespace std;
 class Ray
 {
 public:
+  Ray(const Vector3f &orig, const Vector3f &dir) {
+    origin = orig;
+    direction = dir;
+  }
 
-    Ray( const Vector3f& orig, const Vector3f& dir )
-    {
-        origin = orig;
-        direction = dir;
-    }
+  Ray(const Ray &r) {
+    origin = r.origin;
+    direction = r.direction;
+  }
 
-    Ray( const Ray& r )
-    {
-        origin = r.origin;
-        direction = r.direction;
-    }
+  const Vector3f &getOrigin() const { return origin; }
 
-    const Vector3f& getOrigin() const
-    {
-        return origin;
-    }
+  const Vector3f &getDirection() const { return direction; }
 
-    const Vector3f& getDirection() const
-    {
-        return direction;
-    }
-
-    Vector3f pointAtParameter( float t ) const
-    {
-        return origin + direction * t;
-    }
+  Vector3f pointAtParameter(float t) const { return origin + direction * t; }
 
 private:
-
     // don't use this constructor
-    Ray()
-    {
-        assert( false );
-    }
+  Ray() { assert(false); }
 
-    Vector3f origin;
-    Vector3f direction;
+  Vector3f origin;
+  Vector3f direction;
 
 };
 
-inline ostream& operator << ( ostream& os, const Ray& r)
-{
-    os << "Ray <" << r.getOrigin() << ", " << r.getDirection() << ">";
-    return os;
+inline ostream &operator<<(ostream &os, const Ray &r) {
+  os << "Ray <" << r.getOrigin() << ", " << r.getDirection() << ">";
+  return os;
 }
 
 #endif // RAY_H
--- four/Sphere.h
+++ four/Sphere.h
@@ -2,8 +2,8 @@
 #define SPHERE_H
 
 #include "Object3D.h"
-#include <vecmath/vecmath.h>
 #include <cmath>
+#include <vecmath/vecmath.h>
 
 #include <iostream>
 using namespace std;
@@ -20,7 +20,34 @@ public:
   ~Sphere() { delete material; }
 
   virtual bool intersect(const Ray &r, Hit &h, float tmin) {
+    auto Ro = r.getOrigin();
+    auto Rd = r.getDirection();
+    auto b = Ro - center_;
 
+    float A = Rd.absSquared();
+    float B = 2 * Vector3f::dot(Rd, b);
+    float C = b.absSquared() - radius_ * radius_;
+    float delta = B * B - 4 * A * C;
+
+    if (delta < 0) {
+      return false;
+    }
+
+    auto t1 = (-B - sqrt(delta)) / A / 2;
+    auto t2 = (-B + sqrt(delta)) / A / 2;
+    if (t2 < tmin) {
+      return false;
+    }
+
+    auto t = t1 >= tmin ? t1 : t2;
+    if (t > h.getT()) {
+      return true;
+    }
+
+    auto P = Ro + t * Rd;
+    auto n = (P - center_).normalized();
+    h.set(t, material, n);
+    return true;
   }
 
 protected:
