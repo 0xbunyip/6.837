197693ba904369b74acd4233a10802026d33e853
baodc <chiemduybao@gmail.com>
2021-07-11 16:09:07 +0200

    Triangle.

--- four/Transform.h
+++ four/Transform.h
@@ -3,24 +3,19 @@
 
 #include <vecmath/vecmath.h>
 #include "Object3D.h"
-///TODO implement this class
-///So that the intersect function first transforms the ray
-///Add more fields as necessary
-class Transform: public Object3D
-{
-public: 
+
+class Transform: public Object3D {
+public:
   Transform(){}
- Transform( const Matrix4f& m, Object3D* obj ):o(obj){
-    
-  }
-  ~Transform(){
-  }
-  virtual bool intersect( const Ray& r , Hit& h , float tmin){
-    o->intersect( r , h , tmin);
+  Transform(const Matrix4f &m, Object3D *obj) : o(obj) {}
+  ~Transform(){ }
+
+  virtual bool intersect(const Ray &r, Hit &h, float tmin) {
+    o->intersect(r, h, tmin);
   }
 
- protected:
-  Object3D* o; //un-transformed object	
+protected:
+  Object3D *o; // un-transformed object
 };
 
 #endif //TRANSFORM_H
--- four/Triangle.h
+++ four/Triangle.h
@@ -6,28 +6,60 @@
 #include <cmath>
 #include <iostream>
 
+#include "util.h"
+
 using namespace std;
-///TODO: implement this class.
-///Add more fields as necessary,
-///but do not remove hasTex, normals or texCoords
-///they are filled in by other components
-class Triangle: public Object3D
-{
+class Triangle : public Object3D {
 public:
-	Triangle();
-        ///@param a b c are three vertex positions of the triangle
-	Triangle( const Vector3f& a, const Vector3f& b, const Vector3f& c, Material* m):Object3D(m){
-          hasTex = false;
-	}
+  Triangle(); ///@param a b c are three vertex positions of the triangle
 
-	virtual bool intersect( const Ray& ray,  Hit& hit , float tmin){
-		
-	}
-	bool hasTex;
-	Vector3f normals[3];
-	Vector2f texCoords[3];
+  Triangle(const Vector3f &a, const Vector3f &b, const Vector3f &c, Material *m)
+      : Object3D(m), a_(a), b_(b), c_(c) {
+    hasTex = false;
+    ab_ = b - a;
+    ac_ = c - a;
+  }
+
+  virtual bool intersect(const Ray &ray, Hit &h, float tmin) {
+    auto rdir = ray.getDirection();
+    auto M = Matrix3f(-ab_.x(), -ac_.x(), rdir.x(), //
+                      -ab_.y(), -ac_.y(), rdir.y(), //
+                      -ab_.z(), -ac_.z(), rdir.z());
+
+    if (abs(M.determinant()) < 1e-6) {
+      return false;
+    }
+
+    auto ro = ray.getOrigin();
+    auto y = a_ - ro;
+    auto x = M.inverse() * y;
+    float beta = x[0], gamma = x[1], t = x[2];
+
+    if (t < tmin) {
+      return false;
+    }
+
+    if (beta < 0.0 || beta > 1.0 || gamma < 0.0 || gamma > 1.0 ||
+        (beta + gamma > 1.0)) {
+      return false;
+    }
+
+    if (t > h.getT()) {
+      return true;
+    }
+
+    auto n = (1.0f - beta - gamma) * a_ + beta * b_ + gamma * c_;
+    n.normalized();
+    h.set(t, material, n);
+    return true;
+  }
+
+  bool hasTex;
+  Vector3f normals[3];
+  Vector2f texCoords[3];
+
 protected:
-
+  Vector3f a_, b_, c_, ab_, ac_;
 };
 
 #endif //TRIANGLE_H
