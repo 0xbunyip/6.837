ed26d73c1081556fe30fe81b3e8213da5f0e7df9
baodc <chiemduybao@gmail.com>
2021-07-13 20:03:55 +0200

    Intersect with transform.

--- four/Transform.h
+++ four/Transform.h
@@ -1,21 +1,35 @@
 #ifndef TRANSFORM_H
 #define TRANSFORM_H
 
-#include <vecmath/vecmath.h>
 #include "Object3D.h"
+#include "util.h"
+#include <vecmath/vecmath.h>
 
 class Transform: public Object3D {
 public:
   Transform(){}
-  Transform(const Matrix4f &m, Object3D *obj) : o(obj) {}
+  Transform(const Matrix4f &m, Object3D *obj) : o(obj), m_(m) {
+    mi = m_.inverse();
+  }
   ~Transform(){ }
 
   virtual bool intersect(const Ray &r, Hit &h, float tmin) {
-    o->intersect(r, h, tmin);
+    Vector4f originInObjectSpace = mi * Vector4f(r.getOrigin(), 1.0);
+    Vector4f dirInObjectSpace = mi * Vector4f(r.getDirection(), 0.0);
+    Ray r2(originInObjectSpace.xyz(), dirInObjectSpace.xyz());
+
+    bool intersected = o->intersect(r2, h, tmin);
+
+    const Vector4f normalInWorldSpace =
+        mi.transposed() * Vector4f(h.getNormal(), 0.0);
+    h.set(h.getT(), h.getMaterial(), normalInWorldSpace.xyz());
+
+    return intersected;
   }
 
 protected:
   Object3D *o; // un-transformed object
+  Matrix4f m_, mi;
 };
 
 #endif //TRANSFORM_H
--- four/main.cpp
+++ four/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char *argv[]) {
   // argNum is initialized to 1 because the first
   // "argument" provided to the program is actually the
   // name of the executable (in our case, "a4").
-  int inputIdx, sizeIdx, outputIdx, depthIdx;
+  int inputIdx = -1, sizeIdx = -1, outputIdx = -1, depthIdx = -1;
   for (int argNum = 1; argNum < argc; ++argNum) {
     std::cout << "Argument " << argNum << " is: " << argv[argNum] << std::endl;
     std::string arg = argv[argNum];
